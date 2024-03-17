42f9186ce88d348a28b9cf8c7bef762a5a6b980a
Mitch Zanting <mitch1998@live.nl>
2024-03-17 22:05:46 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,12 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <script>InitPoodlApp('poodl-root', {
+  enableDarkMode: true,
+  overrides: {
+    SwapCardTitle: "LakeView Swap"
+  }
+})</script>
         </p>
       </main>
 
